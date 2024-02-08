85c81d699b093a7495832c4a8a58afbfe4e33477
Natha Chiu <81452446+MaranathaC@users.noreply.github.com>
2024-02-07 16:03:31 -0800

    header and some more

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,42 @@
 import { Hero } from "./components/Hero";
-import { motion, useScroll } from "framer-motion"
+import { motion, useScroll } from "framer-motion";
+import Example from "./components/Header";
+import "./index.css";
+
 
 export default function App() {
   const { scrollYProgress } = useScroll();
 
   return (
     <>
-      <div className="absolute top-0 h-10 w-full bg-blue"></div>
+      <Example />
+      {/* <div className="absolute top-0 h-10 w-full bg-blue"></div> */}
       <motion.div
         className="fixed top-0 left-0 right-0 h-2 bg-gradient-to-r from-pink via-purple via-blue to-cyan origin-left"
         style={{ scaleX: scrollYProgress }}
       />
       <Hero />
-      <div className="flex justify-center">
-        <div className="lg:w-1/2 w-full font-mono text-xl lg:text-3xl text-center text-cyan rounded-lg border-2 border-pink">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod,
-          nunc at pellentesque elementum, risus nisl cursus erat, in condimentum
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod,
-          nunc at pellentesque elementum, risus nisl cursus erat, in condimentum
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod,
-          nunc at pellentesque elementum, risus nisl cursus erat, in condimentum
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod,
-          nunc at pellentesque elementum, risus nisl cursus erat, in condimentum
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod,
-          nunc at pellentesque elementum, risus nisl cursus erat, in condimentum
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod,
-          nunc at pellentesque elementum, risus nisl cursus erat, in condimentum
+      <div className="flex justify-center h-screen">
+        <div className="w-full overflow-hidden">
+          <div className="fade"></div>
+          <section className="star-wars">
+            <div className="crawl">
+              <div className="title">
+                <p>UWB HACKS AI</p>
+                <h1>2024</h1>
+              </div>
+              <p>
+              UWB Hacks In Person is the 7th annual hackathon at the University of Washington, Bothell. 
+              The UWB ACM intends to bring students with diverse skill sets together to make connections and create something useful and/or fun.
+              </p>
+              <p>
+              Our hackathon is a great way for college students to network, get hands-on experience in programming, and solve problems in teams. 
+              Whether this is your first or hundredth hackathon, come join UWB Hacks In Person this year for a fun time!
+              </p>
+            </div>
+          </section>
         </div>
       </div>
-
       <div className="mt-5 bottom-0 h-10 w-full bg-blue"></div>
     </>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,128 @@
+import { Fragment, useState } from 'react'
+import { Dialog, Popover, Transition } from '@headlessui/react'
+import {
+  Bars3Icon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline'
+
+export default function Example() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <header className="bg-black">
+      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className='text-white'>UWB HACKS</span>
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <Bars3Icon className="h-6 w-6 text-white" />
+          </button>
+        </div>
+        <Popover.Group className="hidden lg:flex lg:gap-x-12">
+          <Popover className="relative">
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+            </Transition>
+          </Popover>
+
+          <a href="#" className="text-sm font-semibold leading-6 text-white">
+            About
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-white">
+            Tracks
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-white">
+            FAQ
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-white">
+            Devpost
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-white">
+            Discord
+          </a>
+        </Popover.Group>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <a href="#" className="text-sm font-semibold leading-6 text-white">
+            Log in <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div>
+      </nav>
+      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+                <span className='text-white'>UWB HACKS</span>
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6 text-white" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-pink"
+                >
+                  About
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-purple"
+                >
+                  Tracks
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-blue"
+                >
+                  FAQ
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-blue"
+                >
+                  Devpost
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-blue"
+                >
+                  Discord
+                </a>
+              </div>
+              <div className="py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-cyan"
+                >
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
+  )
+}
